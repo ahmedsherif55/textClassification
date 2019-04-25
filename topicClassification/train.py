173b3aad8d605f9253616a937c9347e35446d1e8
touficy <touficy@optimalsolutions.it>
2019-04-25 20:43:55 +0200

    samir SVM

--- topicClassification/train.py
+++ topicClassification/train.py
@@ -1,5 +1,5 @@
 from preprocessing import PreProcessing
-from Models import NaiveBayes
+from SVM import SVM
 from sklearn.model_selection import train_test_split
 
 data = PreProcessing()
@@ -9,5 +9,5 @@ X = data['post']
 y = data['tags']
 # Split to 20% test data and 80% training data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-nb = NaiveBayes(X_train, X_test, y_train, y_test, data.tags)
-nb.train()
+modle = SVM(X_train, X_test, y_train, y_test, data.tags)
+modle.train()
