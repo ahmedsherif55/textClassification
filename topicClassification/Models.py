ceb3eb1e3dede7513cd5caf03072c592f0f9ab6d
AhmedKhaledAbdalla <ahmedkhaledabdalla97@gmail.com>
2019-04-25 21:38:26 +0200

    logisyic Regression
    Co-Authored-By: khalidamin51 <khalidamin51@users.noreply.github.com>

--- topicClassification/Models.py
+++ topicClassification/Models.py
@@ -5,7 +5,7 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_validate
 import numpy as np
-
+from sklearn.linear_model import LogisticRegression
 
 class NaiveBayes:
     def __init__(self, X_train, X_test, y_train, y_test, tags):
@@ -46,3 +46,48 @@ class NaiveBayes:
 
         #print('accuracy %s' % precision_score(self.y_test, y_pred, average=None))
         print(classification_report(self.y_test, y_pred, target_names=self.tags))
+        
+class logisticR:
+    def __init__(self, X_train, X_test, y_train, y_test, tags):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.tags = tags
+        self.model = Pipeline([('vect', CountVectorizer()),
+                       ('tfidf', TfidfTransformer()),
+                       ('clf', MultinomialNB()),
+                       ])
+
+    def train(self):          
+        
+        logreg = Pipeline([('vect', CountVectorizer()),
+                        ('tfidf', TfidfTransformer()),
+                        ('clf', LogisticRegression(n_jobs=1, C=1e5)),
+                       ])
+        logreg.fit(self.X_train, self.y_train)
+        scoring = {'acc': 'accuracy',
+                   'prec_macro': 'precision_macro',
+                   'rec_micro': 'recall_macro'}
+        scores = cross_validate(self.model,
+                                self.X_train,
+                                self.y_train,
+                                cv=10,
+                                scoring = scoring,
+                                return_train_score=True)
+        for metric_name in scores.keys():
+            average_score = np.average(scores[metric_name])
+            print('%s : %f' % (metric_name, average_score))
+
+        #nb.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+
+        #print('accuracy %s' % precision_score(self.y_test, y_pred, average=None))
+        print(classification_report(self.y_test, y_pred, target_names=self.tags))
+
+        
+        y_pred = logreg.predict(self.X_test)
+        
+        print('accuracy %s' % self.accuracy_score(y_pred, self.y_test))
+        print(classification_report(self.y_test, y_pred,target_names=self.tags))
+        
--- topicClassification/train.py
+++ topicClassification/train.py
@@ -1,6 +1,7 @@
 from preprocessing import PreProcessing
-from SVM import SVM
+#from Models import NaiveBayes
 from sklearn.model_selection import train_test_split
+from Models import logisticR
 
 data = PreProcessing()
 #data.plot_figures()
@@ -9,5 +10,8 @@ X = data['post']
 y = data['tags']
 # Split to 20% test data and 80% training data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-modle = SVM(X_train, X_test, y_train, y_test, data.tags)
-modle.train()
+#nb = NaiveBayes(X_train, X_test, y_train, y_test, data.tags)
+#nb.train()
+
+lg = logisticR(X_train, X_test, y_train, y_test, data.tags)
+lg.train()
