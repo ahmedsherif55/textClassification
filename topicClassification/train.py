d094d9ee3a6175c2323ecc2b4cceb6a6b1a467f8
Ahmed Sherif <31082945+ahmedsherif55@users.noreply.github.com>
2019-04-25 19:18:45 +0200

    Adding Models and train files
    Adding train.py and NB Model in models file
    
    Co-Authored-By: ahmedkhaledabdalla <ahmedkhaledabdalla@users.noreply.github.com>

---
+++ topicClassification/train.py
@@ -0,0 +1,13 @@
+from preprocessing import PreProcessing
+from Models import NaiveBayes
+from sklearn.model_selection import train_test_split
+
+data = PreProcessing()
+#data.plot_figures()
+data = data.clean_text()
+X = data['post']
+y = data['tags']
+# Split to 20% test data and 80% training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+nb = NaiveBayes(X_train, X_test, y_train, y_test, data.tags)
+nb.train()
